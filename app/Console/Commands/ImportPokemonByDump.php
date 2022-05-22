964a73625d3f8c6830b390d27cfb2890ac36d65d
BertVerachtert <104037064+BertVerachtert97@users.noreply.github.com>
2022-05-22 14:24:27 +0200

    import command for pokemons by dump

---
+++ app/Console/Commands/ImportPokemonByDump.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\PokeapiSingularImportHelper;
+use Illuminate\Console\Command;
+
+class ImportPokemonByDump extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pokemon:import-dump';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports pokemons by dump';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if (!file_exists(public_path() . '/pokemons.json')) {
+            $this->error('Could not find file in ' . public_path() . ' with the name pokemons.json');
+            return 0;
+        }
+
+        ini_set('memory_limit', -1);
+        $pokemons = json_decode(file_get_contents(public_path() . '/pokemons.json'), true);
+        $importHelper = new PokeapiSingularImportHelper();
+
+        $bar = $this->output->createProgressBar(count($pokemons));
+
+        foreach ($pokemons as $pokemon) {
+            $name = $importHelper->importPokemon($pokemon);
+
+            $this->info('Succesfully import pokemon with name: ' . $name);
+            $bar->advance();
+        }
+
+        $bar->finish();
+        $this->info('Import has been successful');
+
+        return 0;
+    }
+}
